8c7337380318dfd72afbdddbceb12c9231e620d3
sergioldbm <160510179+sergioldbm@users.noreply.github.com>
2024-02-20 11:50:16 -0300

    adding log info to zipHelp

--- vars/helpZipPackageFolder.groovy
+++ vars/helpZipPackageFolder.groovy
@@ -1,5 +1,7 @@
 def call(String rootFolder, String packageFolder){
     def path = "${rootFolder}\\${packageFolder}\\*"
     helpMsgbox("Zipping package folder content (${path})")
+    echo "Zipping package folder content (${path})"
+    echo " Destination Path ${rootFolder}\\${packageFolder}.zip"
     powershell(returnStdout: true, script: "Compress-Archive -Path ${path} -DestinationPath ${rootFolder}\\${packageFolder}.zip -Force")
 }
