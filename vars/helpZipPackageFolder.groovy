7cb72b466e3b6d3f7c7b9c6f26da9c330ddd1010
sergioldbm <160510179+sergioldbm@users.noreply.github.com>
2024-02-20 11:57:35 -0300

    add log

--- vars/helpZipPackageFolder.groovy
+++ vars/helpZipPackageFolder.groovy
@@ -1,6 +1,7 @@
 def call(String rootFolder, String packageFolder){
     def path = "${rootFolder}\\${packageFolder}\\*"
     helpMsgbox("Zipping package folder content (${path})")
+    echo " Package Folder ${packageFolder}" 
     echo "Zipping package folder content (${path})"
     echo " Destination Path ${rootFolder}\\${packageFolder}.zip"
     powershell(returnStdout: true, script: "Compress-Archive -Path ${path} -DestinationPath ${rootFolder}\\${packageFolder}.zip -Force")
